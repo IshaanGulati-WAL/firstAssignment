ce8eec83fd0f0e060b6a2b32333ecd3a0f57ab40
nagarajWAL <nagarajWAL@users.noreply.github.com>
2020-06-04 11:10:20 +0530

    added signin,signup routes

--- router/index.js
+++ router/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const SignUp=require('../controller/signUp');
+const SignIn=require('../controller/signIn');
+
+router.use('/signUp',SignUp);
+router.use('/signIn',SignIn);
 
 module.exports = exports = router;
