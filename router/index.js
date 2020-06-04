576b1f5af7c6070cef8e64eeec54b561f65b09f5
nagarajWAL <nagarajWAL@users.noreply.github.com>
2020-06-04 16:15:00 +0530

    added recent routes

--- router/index.js
+++ router/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 const SignUp=require('../controller/signUp');
 const SignIn=require('../controller/signIn');
+const AddCourse=require('../controller/addCourses');
+const GetCourse=require('../controller/getCourses');
+const AddTopic=require('../controller/addTopics');
+const GetTopic=require('../controller/getTopics');
 
 router.use('/signUp',SignUp);
 router.use('/signIn',SignIn);
+
+router.use('/addTopic',AddTopic);
+router.use('/getTopic',GetTopic);
+
+router.use('/addCourse',AddCourse);
+router.use('/getCourse',GetCourse);
+
 
 module.exports = exports = router;
