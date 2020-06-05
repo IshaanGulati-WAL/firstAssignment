27b5eb45d2b4e7078404a7dda9d4bbb778157144
nagarajWAL <nagarajWAL@users.noreply.github.com>
2020-06-05 13:30:11 +0530

    jwt verify middleware

--- controller/jwtDecode.js
+++ controller/jwtDecode.js
@@ -1,9 +1,19 @@
 const jwt = require('jsonwebtoken');
+const userModel = require('../models/users');
 
 const jwtDecode = async (req, res, next) => {
     try {
-        req.token = jwt.verify(req.headers['token'], process.env.JWT_SECRET_TOKEN);
-        next();
+        req.token = jwt.verify(req.headers['token'], process.env.JWT_SECRET_TOKEN);//check for validity in the user base and 
+        const user = await userModel.query()
+            .findOne({ email: req.body.email });
+        if (user.id == req.token.id) {
+            console.log(req.token.id)
+            next();
+        } else {
+            res.status(403).json({
+                error: 'User not found',
+            });
+        }
     } catch (err) {
         next(err);
     }
