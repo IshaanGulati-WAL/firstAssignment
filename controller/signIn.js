4dd8a3c61a269805fef1c785f480c8ff24211c5c
nagarajWAL <nagarajWAL@users.noreply.github.com>
2020-06-05 16:44:58 +0530

    changed from password-hash to bcrypt

--- controller/signIn.js
+++ controller/signIn.js
@@ -1,26 +1,18 @@
-
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const passwordHash = require('password-hash');
+
 const userModel = require('../models/users');
 
 async function signInUser(req, res, next) {
     try {
         const user = await userModel.query()
             .findOne({ email: req.body.email });
-        console.log(!(user))
         if (user) {
-            console.log(true)
             let verifyPassword = false;
-            // verifyPassword = passwordHash.verify(req.body.password, user.password);
-            bcrypt.compare(req.body.password, user.password, function (err, result) {
-                verifyPassword = result;
-            })
-            // let verifyPassword = bcrypt.compareSync(req.body.password, user.password);//use async
+            verifyPassword = await bcrypt.compare(req.body.password, user.password)
             if (verifyPassword) {
-                const token = jwt.sign({ id: user.id,email:user.email },
+                const token = jwt.sign({ id: user.id, email: user.email },
                     process.env.JWT_SECRET_TOKEN);
-
                 return res.json({
                     success: true,
                     user: {
